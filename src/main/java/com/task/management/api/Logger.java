1b0417dbff84c5d7449c23dbd94290af460278e8
Joao Oliveira <Joao.Oliveira7@br.ey.com>
2024-05-27 16:10:34 -0300

    Refactor

--- src/main/java/com/task/management/api/Logger.java
+++ src/main/java/com/task/management/api/Logger.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class Logger {
@@ -11,7 +12,8 @@ public class Logger {
     public static PrintStream getPrintStream() throws FileNotFoundException {
         File file = new File("logs");
         file.mkdirs();
-        final String fileName = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_hh-mm-ss-a")) + ".txt";
+        final String fileName = LocalDateTime.now(ZoneId.of("America/Sao_Paulo"))
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd_hh-mm-ss-a")) + ".txt";
         return new PrintStream(new File(file.getAbsolutePath() + "/" + fileName));
     }
 }
