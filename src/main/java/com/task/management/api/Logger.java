33257d233c7a49573fc0177dfb09dae6f4c7c998
Joao Oliveira <Joao.Oliveira7@br.ey.com>
2024-05-27 10:27:35 -0300

    Adding output file

---
+++ src/main/java/com/task/management/api/Logger.java
@@ -0,0 +1,17 @@
+package com.task.management.api;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Logger {
+
+    public static PrintStream getPrintStream() throws FileNotFoundException {
+        File file = new File("logs");
+        file.mkdirs();
+        final String fileName = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_hh-mm-ss-a")) + ".txt";
+        return new PrintStream(new File(file.getAbsolutePath() + "/" + fileName));
+    }
+}
--- src/main/java/com/task/management/api/TaskManagementApiApplication.java
+++ src/main/java/com/task/management/api/TaskManagementApiApplication.java
@@ -3,11 +3,13 @@ package com.task.management.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileNotFoundException;
+
 @SpringBootApplication
 public class TaskManagementApiApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setOut(Logger.getPrintStream());
         SpringApplication.run(TaskManagementApiApplication.class, args);
     }
-
 }
